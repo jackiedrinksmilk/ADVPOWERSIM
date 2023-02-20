58a567058fbd8ee0359b77e37691cefe862087d1
jackiedrinksmilk <106033872+jackiedrinksmilk@users.noreply.github.com>
2023-02-20 14:39:33 -0500

    Delete milestone1_conductorType.py

--- milestone1_conductorType.py
+++
@@ -1,5 +0,0 @@
-class ConductorType:
-    def __init__(self, name: str, numbundles: int, bundlespacing: float):
-        self.name: str = name
-        self.numbundles: int = numbundles
-        self.bundlespacing: float = bundlespacing
